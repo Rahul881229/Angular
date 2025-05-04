f21cfe673dfec527e131da39f87f4953e2b4c39c
Rahul Kumar <rahul.kumar@trinitymobility.com>
2025-05-04 20:02:27 +0530

    directives (ng if and ng for )

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -9,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class DirectivesComponent {
 show =true
+ login="false;"
  Students=["rahul", "anil","ankit", "ankush","mohan"]
+ StudentsData=[
+  {name:"rahul",
+  age:23,job:"developer"},
+  {name:"rahul",age:23,job:"developer"},
+  {name:"ram",age:13,job:"developer"},
+  {name:"rakesh",age:29,job:"developer"},
+  {name:"shyam",age:34,job:"developer"},
+  {name:"raghu",age:23,job:"developer"},]
+  StudentsDatas = [
+    {
+      name: 'Alice',
+      age: 22,
+      job: 'Engineer',
+      skills: ['Angular', 'TypeScript', 'HTML']
+    },
+    {
+      name: 'Bob',
+      age: 24,
+      job: 'Designer',
+      skills: ['Figma', 'Photoshop']
+    }
+  ];
+  
 
+
+  //multiple blocks
+  Block=0;
+  updateBlock(){
+    this.Block++;
+    if( this.Block>3){
+ this.Block=0;
+    }
+  }
 }
